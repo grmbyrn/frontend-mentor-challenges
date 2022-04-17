ffd481bc1df14d6b71df943c31b42cf5a86d3a26
grmbyrn <graemebyrne_89@hotmail.com>
2022-04-17 10:38:52 +0100

    added article preview challenge

---
+++ article-preview/src/index.js
@@ -0,0 +1,20 @@
+const openModal = document.getElementById('open-modal')
+const closeModal = document.getElementById('close-modal')
+const overlay = document.getElementById('overlay')
+
+openModal.addEventListener('click', function(){
+    openModal.addEventListener('click', function(){
+        overlay.style.display = 'block'
+      })
+    
+    closeModal.addEventListener('click', function(){
+        overlay.style.display = 'none'
+    })
+  })
+  
+//   openModal.addEventListener('click', function(){
+//     overlay.style.display = 'none'
+//   })
+
+// overlay.style.display = 'block'
+
